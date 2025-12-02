0573e5ee51d4cc6477cab2cd259b62f563592942
melrose312 <melrose312@gmail.com>
2025-12-02 15:11:55 -0800

    React Crash Course

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx";
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <div>
+        <input type="text" onChange={(event) => {
+          console.log(event.target.value)
+        }} />
+        <button>Add To Do</button>
+      </div>
+      <div className="todo__wrapper">
+        <Todo title="Finish Frontend Simplified" />
+        <Todo title="Finish Interview Section" />
+        <Todo title="Land a $100k Job" />
+      </div>
+      <Modal title="Are you sure you want to delete?" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import './Todo.css'
+
+function Todo ({title}) {
+    function deleteToDo () {
+        console.log('deleteTodo()', title)
+    }
+    return (
+        <div className ="todo">
+      <p>{ title }</p>
+      <button onClick={deleteToDo}>Delete</button>
+    </div>
+    );
+}
+
+export default Todo;
